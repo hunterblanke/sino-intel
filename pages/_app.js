2b262b51fb4457df51832d3d79c4c2a55e26128a
hunterblanke <hunterblanke@gmail.com>
2026-06-07 00:24:20 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const saved = localStorage.getItem("gwd-theme");
+    if (saved === "dark") {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
